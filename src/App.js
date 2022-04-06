2a25513a9381e2fdf465177328b184c7db367b13
com <x7788@naver.com>
2022-04-06 15:06:10 +0900

    update : props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 
 import MyHeader from './my_header';
 import Counter from './count';
+import Container from './Container';
 
 function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    initialValue: 5,
+  };
+
   return (
-    <div className="App">
-      <MyHeader />
-      <Counter />
-    </div>
+    <Container>
+      <div className="App">
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
--- src/count.js
+++ src/count.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
+import OddEvenResult from './OddEvenResult';
 
-const Counter = () => {
-  // 0에서 출발
-  // 1씩 증가하고
-  // 1씩 감소하는 형태
-
-  console.log('counter 호출!'); // 함수가 다시 호출되는지 확인하기 위해
-
-  const [count, setCount] = useState(0);
+const Counter = ({ initialValue }) => {
+  const [count, setCount] = useState(initialValue);
 
   const onIncrease = () => {
     setCount(count + 1); // 0 + 1
@@ -22,6 +17,7 @@ const Counter = () => {
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
+      <OddEvenResult count={count} />
     </div>
   );
 };
